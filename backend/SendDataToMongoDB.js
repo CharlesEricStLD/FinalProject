bc15458673cf56de28f87c3537da4e1f553bc7ef
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-11-22 13:34:57 -0500

    feat : Add function to add brute data to mongoDb

--- backend/SendDataToMongoDB.js
+++ backend/SendDataToMongoDB.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 const { v4: uuidv4 } = require('uuid');
 
 
-
-
 const centers = [ {
   _id : uuidv4(),
   image : "https://m1.quebecormedia.com/emp/emp/62216354_2615382654865a-73cc-4c33-bc1d-8479a3cd3d50_ORIGINAL.jpg?impolicy=crop-resize&x=0&y=0&w=1000&h=667&width=960",
