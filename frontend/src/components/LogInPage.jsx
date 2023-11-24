4bceebef9b0e7e1c791b592b7de1bb34e198c188
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-11-24 13:43:25 -0500

    Add login page with and logIn as guest with Backend verification

--- frontend/src/components/LogInPage.jsx
+++ frontend/src/components/LogInPage.jsx
@@ -7,45 +7,88 @@ import styled from "styled-components";
 export const LoginPage = () => {
 
   //user name should be context state
-
-
   const navigate = useNavigate();
 
-  const handleSignIn = () => {
-      navigate("/signin")
+  const emptyUser = {
+    username : "", 
+    password : ""
   }
 
-  const [user, SetUser] = useState(null);
-  const [password, setPassword] = useState(null)
+  const [user, setUser] = useState(emptyUser);
+  const [verificationInProgress, setVerificationInProgress] = useState(false);
+  const [validationMessage, setValidationMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
   
-  //FETCH POST
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "username") {
+      const valueStore = value.toLowerCase();
+      setUser({...user,[name] : valueStore})
+    }   
+      setUser({...user, [name] : value})
+      setErrorMessage("");
+      setValidationMessage("");
+  }
 
-  //BAckedn : 
-  //Add user to db with : 
-  // user = {
-    // username : "BOB", (LOWERCASE in BACKEND)
-    // password : "pass1" 
-  //}
-  //
+  const logInVerification = () => {
+    setErrorMessage("");
+    setValidationMessage("");
+    setVerificationInProgress(true);
 
+    fetch("/api/login",{
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({user})
+    })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      setVerificationInProgress(false);
+      if (data.message === "Request sucessfull: ") {
+        setValidationMessage(`Welcome back ${data.data}!, your will be redirected to your User page in a few sec !`)
+        sessionStorage.setItem("user", user.username)
+        
+        setTimeout(() => {
+          navigate(`/user/${user.username}`)
+        }, 3000)
+      } else {
+        setErrorMessage(data.message);
+      }
+    })
+  }
+
+
+  const handleLogIn = (event) => {
+    event.preventDefault();
+    logInVerification()
+  }
+
+
+  const handleLogInAsGuest = (event) => {
+    setUser({username : "Guest1", password : "toyota1525**"})
+    event.preventDefault();
+    logInVerification();
+  }
 
   return (
     <form>
       <h1>LOGIN PAGE </h1>
       <InputContainer>
       <label> UserName :
-      <input type="text"></input> 
+      <input type="text" name="username" onChange={(event) =>handleChange(event)}></input> 
       </label>
       <label> Password
-        <input type="password"></input>
+        <input type="password" name="password" onChange={(event) =>handleChange(event)}></input>
       </label>
-      <button>log in</button>
-      <button onClick = {handleSignIn}>Sign in</button>
-      <button>log in as Guest</button>
+      <button onClick = {handleLogIn} disabled={verificationInProgress}>Log in</button>
+      <button onClick = {handleLogInAsGuest} disabled={verificationInProgress}>log in as Guest</button>
+      <a href="/signin" disabled={verificationInProgress}>Sign In</a>
       </InputContainer>
+      {errorMessage? <p>{errorMessage}</p> : <p>{validationMessage}</p>}
     </form>
-    
-
   )
 
 
