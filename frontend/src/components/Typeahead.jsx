84582781075f1e672cc77d7fe39e7a11e43b9772
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-11-29 16:46:23 -0500

    feat : Add Typehead connection with fetch from Database and redirect to center page when center choosen

--- frontend/src/components/Typeahead.jsx
+++ frontend/src/components/Typeahead.jsx
@@ -1,14 +1,33 @@
 //component of the Typeahead
 import { Combobox } from '@headlessui/react'
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 
 export const Typeahead = ( ) => {
 
-const options = [{name:"TEST", region : "somewhere"}, {name : "test2", region: "canada"}];
+// const options = [{name:"TEST", region : "somewhere"}, {name : "test2", region: "canada"}];
 
+const [options, setOptions] = useState([]);
 const [selectedoption, setSelectedoption] = useState("")
 const [query, setQuery] = useState('')
+
+
+//Fetch from server to get all center
+useEffect(() => {
+
+  fetch("/api/allcentersinformation")
+  .then(response => response.json())
+  .then(data => {
+    if (data.message === "Request sucessfull: ") {
+      setOptions(data.data)
+    } else {
+      console.err(data.message);
+    }
+  })
+
+},[])
+
+
 
 const filteredPeople =
   options.filter((option) => {
@@ -21,7 +40,7 @@ return (
     <Combobox.Options>
       {query? filteredPeople.map((option) => (
         <Combobox.Option key={option.name} value={option.name}>
-          <a href='/admin'>{option.name} {option.region}</a>
+          <a href={`/center/${option._id}`}>{option.name} {option.region}</a>
         </Combobox.Option>
       )) : "" }
     </Combobox.Options>
