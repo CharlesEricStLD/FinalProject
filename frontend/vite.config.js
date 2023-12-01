338eabb1d57dd598c8a76daf008b7b2817a2273a
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-12-01 13:07:20 -0500

    Fix : remove tailwind from project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,12 @@
 // https://vitejs.dev/config/
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
 
 export default defineConfig({
   plugins: [react()],
   server : {
     proxy : {
     "/api" : 'http://localhost:8000',
-    }
-  }, css: {
-    postcss: {
-      plugins: [tailwindcss()],
     }
   }
 })
