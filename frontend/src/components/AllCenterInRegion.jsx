dbb5be3b07f85a6e4baeaffe916acf1de83dd6d8
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-12-02 14:49:41 -0500

    Feat : Add new CenterCard component to AllCenterOfRegion Page

--- frontend/src/components/AllCenterInRegion.jsx
+++ frontend/src/components/AllCenterInRegion.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { CenterCard } from "./CenterCard";
 
 export const AllCenterInRegion = () => {
 
@@ -35,28 +36,7 @@ export const AllCenterInRegion = () => {
     {
       loading ? <p> is loading ....</p> : (
         allCentersOfRegion.map(center => 
-          
-          <CenterCard key={center.name} >
-          
-          <div>
-          <Link to={`/center/${center._id}`}>
-          <img src={center.image}></img>
-          </Link>
-          </div>
-
-          <div>
-          <h2>{center.name}</h2>
-          <p>Region : {center.region} </p>
-          <p> <a href={center.url} target="blank">{center.url}</a></p>
-          
-          <h3>Contact</h3>
-          <p>{center.contact.email}</p>
-          <p>{center.contact.facebook}</p>
-          <p>{center.contact.phone}</p> 
-          </div>
-
-          </CenterCard> 
-          
+          <CenterCard center={center}/> 
         )
       )
     }
@@ -76,40 +56,5 @@ const AllCenterInRegionStyling = styled.div`
   h1{
     font-size: 4em;
     margin-bottom: 1%;
-  }
-`
-
-const CenterCard = styled.div`
-  margin-bottom:5%;
-  padding:2%;
-  width:90%;
-  display: grid;
-  grid-template-columns: 0.5fr 0.5fr;
-  column-gap: 5%;
-  border-radius: 15px;
-  border: solid 2px;
-
-  div:nth-child(1){
-    width:100%;
-    padding:1%;
-
-    img {
-    width:100%;
-    border-radius: 15px;
-  }
-  }
-
-  div:nth-child(2){
-    padding-top: 10%;
-
-    p{
-      margin-top:1%;
-      margin-bottom: 1%;
-    }
-
-    h2 {
-      margin-top:5%;
-      margin-bottom:2%;
-    }
   }
 `
