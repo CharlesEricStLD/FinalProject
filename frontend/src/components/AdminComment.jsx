47d64f1be0ef4fa65bf34f342e70221cdb00447f
CharlesEricStLD <ce.stlouisdupuis+gitHub@gmail.com>
2023-12-04 00:30:45 -0500

    feat : Add style to Admin Page comment

--- frontend/src/components/AdminComment.jsx
+++ frontend/src/components/AdminComment.jsx
@@ -1,6 +1,7 @@
 //component to render each the comment
 import { useContext } from "react"
 import { NewCommentContext } from "../routes/RoutesIndex"
+import styled from "styled-components"
 
 export const AdminComment = ({comment}) => {
 
@@ -20,7 +21,7 @@ export const AdminComment = ({comment}) => {
     }
 
   return (
-    <>
+    <CommentContainer>
     <h1>Comment </h1>
     {comment.accepted?<p style={{color:"green"}}>APPROVED</p>: <p>Waiting For approval...</p>}
     <p>Comment made by {comment.username}</p>
@@ -29,8 +30,15 @@ export const AdminComment = ({comment}) => {
     <p>date : {comment.date}</p>
     <button onClick={handleApproveComment}>Approved</button>
     <button>Declined</button>
-    </>
+    </CommentContainer>
   )
 
 
 }
+
+const CommentContainer = styled.div`
+  margin:2%;
+  padding:2%;
+  border-radius: 15px;
+  border: solid 2px;
+`
